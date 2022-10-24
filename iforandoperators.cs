9072006ac23663cfbf005ec4d4aa877a37a8aaa0
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-10-24 13:40:45 -0400

    Create iforandoperators.cs

---
+++ iforandoperators.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+
+{
+  public static void Main(string[] args)
+  {
+Console.WriteLine((7>5) && (3>5 //and operator
+Console.WriteLine((48>6) || (5<9)); //or operator
+Console.WriteLine(!(7>102)); //not operator 
+Console.WriteLine(7>10);  
+
+  }
+}
