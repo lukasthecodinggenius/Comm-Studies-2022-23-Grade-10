f87b87a12c4bf86ee93741bd6eccf1d667d9023b
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-10-28 14:04:04 -0400

    Create whileloop.cs

---
+++ whileloop.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+		{
+			int i = 0;
+		while (i<10) {
+			Console.WriteLine("Hello world!");
+		}
+		
+												
+              
+
+
+    }
+}
