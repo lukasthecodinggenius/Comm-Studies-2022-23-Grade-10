7ef55aa368c6b57022a87bf5031c5c5d9b0fea6a
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-15 13:20:58 -0500

    Create Trivia-Questions.cs

---
+++ Trivia-Questions.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+Console.WriteLine("Welcome to trivia, try your best to answer the questions:");
+
+Console.WriteLine("Question - What is lukas's last name?");
+
+Console.WriteLine("1. Munch");
+Console.WriteLine("2. Muench");
+Console.WriteLine("3. Menuch");
+Console.WriteLine("4. James");
+
+	string answer1;
+	answer1 = Console.ReadLine();
+
+
+
+
+	
+							
+
+ }
+}
