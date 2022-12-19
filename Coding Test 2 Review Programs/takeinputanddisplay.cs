f03322f83bb609dd67fb396056c34c6bef9ad01e
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-12-19 13:33:07 -0500

    Create takeinputanddisplay.cs

---
+++ Coding Test 2 Review Programs/takeinputanddisplay.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program 
+{
+	public static void Main(string[] args)
+	{
+	int number;
+		Console.WriteLine("What is the number you are thinking of?");
+number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+	
+	}
+}
