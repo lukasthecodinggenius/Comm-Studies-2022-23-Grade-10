f74191b6a8ceece18745a2c6063d183d61dd8457
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-12-19 13:41:22 -0500

    Update areaofpi.cs

--- Coding Test 2 Review Programs/areaofpi.cs
+++ Coding Test 2 Review Programs/areaofpi.cs
@@ -4,12 +4,13 @@ class Program
 {
 	public static void Main(string[] args)
 	{
+	//declare the constant variable (which never changes throughout the program) followed by double because pi contains decimals
 	const double pi = 3.14; 
 	Console.WriteLine("What is the radius of the circle?");
 	int radius;
-
+//take input from the user using toint32 and readline
 	radius = Convert.ToInt32(Console.ReadLine());
-
+//display the area of a circle using the radius given by the user
 	Console.WriteLine("The Area of this circle is: " + radius * radius * pi);
 	
 	}
