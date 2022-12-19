393a4cd773e21032534f9a0aa8ba96bc3b4afbef
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-12-19 13:55:19 -0500

    Create mathoperators.cs

---
+++ Coding Test 2 Review Programs/mathoperators.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program 
+{
+	public static void Main(string[] args)
+	{
+int a = 1;
+double b = 9.34;
+int c = 103;
+
+		Console.WriteLine(b - a * b * b - c);
+		Console.WriteLine(c % b);
+		Console.WriteLine(c / b);
+		Console.WriteLine(b++);
+		Console.WriteLine(b--);
+		Console.WriteLine(++b);
+		Console.WriteLine(--b);
+	}
+}
